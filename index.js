d11f4a1b76d24fa847dafec4584ca3edf4c8c4f4
Koima-Sam <sam17koima@gmail.com>
2022-06-18 13:18:46 +0300

    commit tree 3969d278f5c7822f0e66b6e98e7b8b3260eb2141

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0 ; i<word.length/2; i++){
+    const endIndex = word.length-1-i
+    if(word[i]!=word[endIndex]){
+        return false
+    }
+}
+  return true
 }
 
 /* 
   Add your pseudocode here
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return true
 */
 
 /*
   Add written explanation of your solution here
+  Input: "racecar"
+Input length divided by two: 3.5
+Iteration:
+  Index 0 (less than 3.5, keep iterating)
+  Index 1 (less than 3.5, keep iterating)
+  Index 2 (less than 3.5, keep iterating)
+  Index 3 (less than 3.5, keep iterating)
+  Index 4 (not less than 3.5, stop iterating)
+For "racecar", our loop will iterate up to the middle "e"
+
+Input: "abba"
+Input length divided by two: 2
+Iteration:
+  Index 0 (less than 2, keep iterating)
+  Index 1 (less than 2, keep iterating)
+  Index 2 (not less than 2, stop iterating)
 */
 
 // You can run `node index.js` to view these console logs
